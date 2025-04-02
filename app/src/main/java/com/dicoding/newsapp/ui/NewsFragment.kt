3876551245e54cb962c90aa5101cffd3a1717a7a
zakymzn <mamurzakynurrokhman@gmail.com>
2025-04-02 19:38:24 +0700

    codelab bookmark

--- app/src/main/java/com/dicoding/newsapp/ui/NewsFragment.kt
+++ app/src/main/java/com/dicoding/newsapp/ui/NewsFragment.kt
@@ -32,7 +32,13 @@ class NewsFragment : Fragment() {
             factory
         }
 
-        val newsAdapter = NewsAdapter()
+        val newsAdapter = NewsAdapter { news ->
+            if (news.isBookmarked) {
+                viewModel.deleteNews(news)
+            } else {
+                viewModel.saveNews(news)
+            }
+        }
 
         if (tabName == TAB_NEWS) {
             viewModel.getHeadlineNews().observe(viewLifecycleOwner) { result ->
@@ -41,11 +47,13 @@ class NewsFragment : Fragment() {
                         is Result.Loading -> {
                             binding?.progressBar?.visibility = View.VISIBLE
                         }
+
                         is Result.Success -> {
                             binding?.progressBar?.visibility = View.GONE
                             val newsData = result.data
                             newsAdapter.submitList(newsData)
                         }
+
                         is Result.Error -> {
                             binding?.progressBar?.visibility = View.GONE
                             Toast.makeText(
@@ -56,6 +64,11 @@ class NewsFragment : Fragment() {
                         }
                     }
                 }
+            }
+        } else if (tabName == TAB_BOOKMARK) {
+            viewModel.getBookmarkedNews().observe(viewLifecycleOwner) { bookmarkedNews ->
+                binding?.progressBar?.visibility = View.GONE
+                newsAdapter.submitList(bookmarkedNews)
             }
         }
 
