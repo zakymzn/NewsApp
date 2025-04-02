2d7f924357ce2067200b380c501fbbee6297d52b
zakymzn <mamurzakynurrokhman@gmail.com>
2025-04-02 18:25:13 +0700

    perbaikan api service dan api key

--- app/src/main/java/com/dicoding/newsapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/dicoding/newsapp/data/remote/retrofit/ApiService.kt
@@ -6,6 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("top-headlines?country=id&category=science")
+//    @GET("top-headlines?country=id&category=science")
+    @GET("top-headlines?category=science")
     fun getNews(@Query("apiKey") apiKey: String): Call<NewsResponse>
 }
